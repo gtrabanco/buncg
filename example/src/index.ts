4f2dd7052379cb169159e9886dcea2cfe2f0d830
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2023-09-11 21:08:25 +0200

    Cli

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,7 +1,2 @@
-import { App } from "buncg";
-
-
 console.log(globalThis.buncg); // How to make globalThis types to recognize buncg?
 console.log("Hello! First project with BunCG!");
-App.get("cli", () => "Hello from CLI!")
-
